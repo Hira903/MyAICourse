44af12ff64ab09c85949648eb09725dbadedd751
Hira903 <hiraidrees1702@gmail.com>
2025-08-09 11:22:36 +0500

    #axcessing array using index value

---
+++ Case1a-index.py
@@ -0,0 +1,14 @@
+s = ["Geeks", "for", "Geeks"]
+
+print('Starts here')
+
+# using for loop with string
+for a in (0,2):
+    print (s[a])
+
+print('Mid here')
+
+for i in s:
+    print(i)
+
+print('Close here')
